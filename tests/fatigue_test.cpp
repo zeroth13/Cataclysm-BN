79eff1df7debf5db711b9be08202a95b40c534c6
Coolthulhu <Coolthulhu@gmail.com>
2021-05-22 12:14:30 +0200

    More resilient sleep tests

--- tests/fatigue_test.cpp
+++ tests/fatigue_test.cpp
@@ -8,6 +8,7 @@
 #include "catch/catch.hpp"
 #include "enums.h"
 #include "npc.h"
+#include "options.h"
 
 
 static const efftype_id effect_sleep( "sleep" );
@@ -27,6 +28,7 @@ static void update_body_for( Character &dude, time_duration time )
 
 TEST_CASE( "must_sleep_8_hours", "[fatigue]" )
 {
+    REQUIRE( !get_option<bool>( "NO_NPC_FOOD" ) );
     standard_npc dude( "sleeper", tripoint( -1, -1, 0 ) );
     dude.set_fatigue( 0 );
     WHEN( "The character is active for 16 hours" ) {
@@ -36,6 +38,9 @@ TEST_CASE( "must_sleep_8_hours", "[fatigue]" )
         AND_WHEN( "The character sleeps for 7 hours and 30 minutes" ) {
             dude.add_effect( effect_sleep, 24_hours );
             update_body_for( dude, 7_hours + 30_minutes );
+            THEN( "The character is still sleeping" ) {
+                CHECK( dude.has_effect( effect_sleep ) );
+            }
             THEN( "The character is not fully rested" ) {
                 CHECK( dude.get_fatigue() > 0 );
             }
@@ -46,12 +51,16 @@ TEST_CASE( "must_sleep_8_hours", "[fatigue]" )
             THEN( "The character is fully rested" ) {
                 CHECK( dude.get_fatigue() == 0 );
             }
+            THEN( "The character is awake" ) {
+                CHECK( !dude.has_effect( effect_sleep ) );
+            }
         }
     }
 }
 
 TEST_CASE( "sleep_deprivation_rate", "[fatigue]" )
 {
+    REQUIRE( !get_option<bool>( "NO_NPC_FOOD" ) );
     standard_npc dude( "sleeper", tripoint( -1, -1, 0 ) );
     dude.set_fatigue( 0 );
     dude.set_sleep_deprivation( 0 );
@@ -62,6 +71,9 @@ TEST_CASE( "sleep_deprivation_rate", "[fatigue]" )
         AND_WHEN( "The character sleeps for 5 hours and 30 minutes" ) {
             dude.add_effect( effect_sleep, 24_hours );
             update_body_for( dude, 5_hours + 30_minutes );
+            THEN( "The character is still sleeping" ) {
+                CHECK( dude.has_effect( effect_sleep ) );
+            }
             THEN( "The character has some sleep deprivation" ) {
                 CHECK( dude.get_sleep_deprivation() > 0 );
             }
